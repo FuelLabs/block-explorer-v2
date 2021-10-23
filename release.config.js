606b254c242e504833bb029cacbb73c8d2e64813
alex-matthe <alex.matthekub@gmail.com>
2021-10-24 01:26:42 +0300

    Add develop as release branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "branches": ['+([0-9])?(.{+([0-9]),x}).x', 'master', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
+  "branches": ['+([0-9])?(.{+([0-9]),x}).x', 'master', 'next', 'develop', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
