1edabcb7576f34dca61e291e08cd9385f35cc01b
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-05-11 01:02:29 -0300

    feat: add number format to big numbers

---
+++ src/utils/bigNumber.ts
@@ -0,0 +1,9 @@
+import type { BigNumberish } from '@ethersproject/bignumber';
+import { commify, formatUnits, parseUnits } from '@ethersproject/units';
+
+export const parseToFormattedNumber = (value: string | BigNumberish, unit?: BigNumberish) => {
+  const bigNumber = typeof(value) === "string" ? parseUnits(value, unit) : value;
+ 
+  return commify(formatUnits(bigNumber, unit));
+}
+  
