427533357fab729404700e7d3017f67ae1105925
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-05-11 02:10:56 -0300

    chore: create const to hold decimal units

--- src/utils/bigNumber.ts
+++ src/utils/bigNumber.ts
@@ -1,7 +1,8 @@
 import type { BigNumberish } from '@ethersproject/bignumber';
 import { commify, formatUnits, parseUnits } from '@ethersproject/units';
 
+export const DECIMAL_UNITS = 3;
 export const parseToFormattedNumber = (
   value: string | BigNumberish,
-  unit: BigNumberish = 3
+  unit: BigNumberish = DECIMAL_UNITS
 ) => commify(formatUnits(value, unit));
