c37352e9f8aec296f882d232e7fe90adc466fd5a
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-05-11 01:53:41 -0300

    fix: big number formatting

--- src/utils/bigNumber.ts
+++ src/utils/bigNumber.ts
@@ -1,8 +1,8 @@
 import type { BigNumberish } from '@ethersproject/bignumber';
 import { commify, formatUnits, parseUnits } from '@ethersproject/units';
 
-export const parseToFormattedNumber = (value: string | BigNumberish, unit?: BigNumberish) => {
-  const bigNumber = typeof value === 'string' ? parseUnits(value, unit) : value;
+export const parseToFormattedNumber = (value: string | BigNumberish, unit: BigNumberish = 'kwei') => {
+  const bigNumber = typeof value === 'string' ? parseUnits(value, 0) : value;
 
   return commify(formatUnits(bigNumber, unit));
 };
