4b5dd68934005e379fa8ca2e428cb95b69582a2f
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-05-11 01:22:45 -0300

    chore: prettier

--- src/utils/bigNumber.ts
+++ src/utils/bigNumber.ts
@@ -2,8 +2,7 @@ import type { BigNumberish } from '@ethersproject/bignumber';
 import { commify, formatUnits, parseUnits } from '@ethersproject/units';
 
 export const parseToFormattedNumber = (value: string | BigNumberish, unit?: BigNumberish) => {
-  const bigNumber = typeof(value) === "string" ? parseUnits(value, unit) : value;
- 
+  const bigNumber = typeof value === 'string' ? parseUnits(value, unit) : value;
+
   return commify(formatUnits(bigNumber, unit));
-}
-  
+};
