23aa533a321a7b9a04e7d344e054b5b9ea9c0e8e
Luiz Estácio <luizstacio@gmail.com>
2023-08-08 19:13:22 -0300

    feat: change protocol versions load

--- craco.config.js
+++ craco.config.js
@@ -8,7 +8,9 @@ const protocol = packageJson.protocol;
 const BASE_URL = path.join(process.env.REPOSITORY_NAME || '', protocol);
 
 process.env.REACT_APP_REPOSITORY_NAME = process.env.REPOSITORY_NAME || '';
-process.env.PUBLIC_URL = BASE_URL;
+if (process.env.NODE_ENV === 'production') {
+  process.env.PUBLIC_URL = BASE_URL;
+}
 
 const { BundleStatsWebpackPlugin } = require('bundle-stats-webpack-plugin');
 const CopyPluginWebpack = require('copy-webpack-plugin');
@@ -76,6 +78,10 @@ module.exports = () => {
               .replace('%REPOSITORY_NAME%', process.env.REPOSITORY_NAME || '')
               .replace('%PROTOCOL_NAME%', packageJson.protocol);
           },
+        },
+        {
+          from: './public/protocol-versions.json',
+          to: '../',
         },
       ],
     }),
--- src/providers/NetworkProvider/index.tsx
+++ src/providers/NetworkProvider/index.tsx
@@ -2,26 +2,28 @@ import { useEffect } from 'react';
 import urlJoin from 'url-join';
 
 import { httplink } from '../../client';
-import protocolVersions from '../../protocol-versions.json';
 
 import { useNetworkInformationQuery } from './__generated__/operations';
 import { getQueryProviderUrl } from './utils/queryProviderUrl';
 import { getProviderUrl, setNetworkVersion, setProviderUrl } from './utils/storage';
 
-const versions: { [key: string]: string } = protocolVersions;
 const { location } = window;
 const { REACT_APP_REPOSITORY_NAME } = process.env;
+const repositoryName = REACT_APP_REPOSITORY_NAME || '/';
 
 export function NetworkProvider({ children }: { children: React.ReactNode }) {
   const { loading } = useNetworkInformationQuery({
-    onCompleted: (data) => {
+    onCompleted: async (data) => {
+      const versions: Record<string, string> = await fetch(
+        urlJoin(repositoryName, '/protocol-versions.json')
+      ).then((res) => res.json());
       const nodeVersion = data.nodeInfo.nodeVersion;
       const [, version] = nodeVersion.split('.');
       setNetworkVersion(nodeVersion);
       const compatibleVersion = Object.keys(versions).find((k) => Number(k) >= Number(version));
       if (compatibleVersion) {
         if (!location.pathname.includes(versions[compatibleVersion])) {
-          const pathname = urlJoin(REACT_APP_REPOSITORY_NAME || '', versions[compatibleVersion]);
+          const pathname = urlJoin(repositoryName, versions[compatibleVersion]);
           location.href = `${location.origin}${pathname}/${location.hash}`;
         }
       }
